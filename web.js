9c7d0db714b9da9910f1abcad10df01914075cf0
Vasil Tashev <vtashev@gmail.com>
2013-07-09 08:16:10 +0000

    web.js reads from indexl.html via bufferl

--- web.js
+++ web.js
@@ -2,8 +2,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+require('buffer');
+var fs = require('fs');
+
+var buf = new Buffer(fs.readFileSync("index.html"));
+
+//Test purpose
+//console.log(buf.toString("utf-8", 0, 28));
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buf.toString("utf-8",0,28))
 });
 
 var port = process.env.PORT || 5000;

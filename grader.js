cf86a6c01d77f2d5115dbf343e68d2e14ca866ec
Vasil Tashev <vtashev@gmail.com>
2013-07-10 15:47:40 +0000

    grader.js now works with URL, do have some timing issues

--- grader.js
+++ grader.js
@@ -22,6 +22,7 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
@@ -35,6 +36,17 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var assertURLExists = function(infile) {
+    var instr = infile.toString();
+    if(!fs.existsSync(instr)) {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return instr;
+};
+
+
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -61,14 +73,31 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_link>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    if (!program.file) {
+            var checkJson = checkHtmlFile(program.file, program.checks);
+          }
+          else
+          {           
+            rest.get(program.url).on('complete', function(result) {   
+                                                              // save html to the file
+                                                              fs.writeFile('web.html',result);                                                      
+                                                                    }
+                                      )              
+
+            var checkJson = checkHtmlFile('web.html', program.checks);          
+          }
+    
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
